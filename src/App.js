d70e4c2f7d7c49406befa6026900ccd97bb6eee1
dylan <dylangthomas1@gmail.com>
2023-02-18 13:49:00 -0700

    please work

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  React.useEffect(() => {
+    fetch('https://api.fantasynerds.com/v1/nfl/depth?apikey=DDRJBZADV6DV68D5VUHE')           
+      .then(response => response.json())
+      .then(data => console.log(data));
+  }, [])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Football</h1>
+      
     </div>
   );
 }
